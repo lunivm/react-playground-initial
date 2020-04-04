e4f9b36dde801bb24f4e44b2fc73975a06fd9551
lunivm <lunivm@yahoo.com>
2020-04-05 00:28:38 +0300

    initial commit

---
+++ frontend/src/components/ImageContainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled  from 'styled-components'
+
+const Image = styled.img`
+  max-width: 100%;
+  max-height: 100%;
+`;
+
+const Div = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+export default function ImageContainer (props) {
+  return <Div data-component="ImageContainer"><Image class="image" alt="" src={props.url}/></Div>;
+}
---
+++ frontend/src/components/card/Card.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Card.css';
+import ImageContainer from '../ImageContainer';
+
+const likeIcon = <svg height="16" viewBox="0 0 16 14" width="18">
+  <g fill="transparent">
+    <path _ngcontent-mev-c29="" className="save-icon"
+      d="m6.84,13.104084c-4.12,-3.563 -6.84,-5.913 -6.84,-8.797c0,-2.35 1.936,-4.196 4.4,-4.196c1.392,0 2.728,0.618 3.6,1.595c0.872,-0.977 2.208,-1.595 3.6,-1.595c2.464,0 4.4,1.846 4.4,4.196c0,2.884 -2.72,5.234 -6.84,8.805l-1.16,0.999l-1.16,-1.007z"
+      fill="currentColor" stroke="currentColor"></path>
+  </g>
+</svg>;
+const cardBgImage = image => ({ backgroundImage: `url(${image})`});
+
+const Header = props => <div class="header-image" style={cardBgImage(props.image)}/>;
+
+const Body = props => (
+  <div class="body">
+    <div class="vendor-logo"><ImageContainer url={props.vendorImage}/></div>
+
+    <h4 class="card-header">{props.header}</h4>
+
+    <div class="actions">
+      <button class="btn like-btn">
+        {likeIcon}
+      </button>
+    </div>
+  </div>
+);
+
+export default class Card extends React.Component {
+  render() {
+    return (
+      <div data-component="Card">
+        <Header image={this.props.image}/>
+        <Body header={this.props.header} vendorImage={this.props.vendorImage}/>
+      </div>
+    );
+  }
+}
