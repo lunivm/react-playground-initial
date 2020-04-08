f2244a500e605e0cd2cef0987ad9f5a6d3770a36
lunivm <lunivm@yahoo.com>
2020-04-08 12:33:53 +0300

    add minor code tweaks

---
+++ frontend/src/core/http.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import getConfig from './config';
+
+export const http = {
+  get(url, queryParams) {
+    return axios.get(`${getConfig('apiUrl')}/${url}`).then(i => i.data);
+  }
+};
--- frontend/src/pages/home/featured.service.js
+++ frontend/src/pages/home/featured.service.js
@@ -1,6 +1,5 @@
-import axios from 'axios';
-import getConfig from '../../core/config';
+import { http } from '../../core/http';
 
 export async function getAllFeatures() {
-  return axios.get(getConfig('apiUrl') + '/featured').then(i => i.data);
+  return http.get('/featured');
 }
